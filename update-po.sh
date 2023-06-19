6a63149f455771f69bcdf3355f777ef99e7260c5
Onur Alp AKIN <onralpakin@gmail.com>
2023-06-19 08:44:33 +0300

    remove everything

--- update-po.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for filename in safeeyes/config/locale/*/LC_MESSAGES/safeeyes.po; do
-    echo "$filename"
-    msgmerge -U -N "$filename" safeeyes/config/locale/safeeyes.pot
-    if [ -f "$filename~" ] ; then
-        rm "$filename~"
-    fi
-done
